144294e57c96599727732a5d34e626e86ec84192
HenrikData <henrik.karlsson2.student@ksgyf.se>
2025-05-16 13:15:55 +0200

    Döp huvudmapp

--- Modul_strukturverktyg.psm1
+++ Modul_strukturverktyg.psm1
@@ -1,6 +1,9 @@
 # Funktion för modulen starta-strukturverktyg
 Function Starta-Strukturverktyg {
 
+# Döp huvudmapp
+    $namn = Read-Host "Ange ett namn på mappen"
+    Write-Host "Du angav: $namn"
 
-    
+
 }
--- Run_strukturverktyg.ps1
+++ Run_strukturverktyg.ps1
@@ -1,5 +1,5 @@
 # Denna fil skapar en mappstruktur och en loggfil baserat på det namn du väljer för huvudmappen.
-Import-Module "$PSScriptRoot\verktyg.psm1"
-Starta-Verktyg  
+Import-Module "$PSScriptRoot\Modul_strukturverktyg.psm1"
+Starta-Strukturverktyg  
 
 # ← din huvudfunktion
