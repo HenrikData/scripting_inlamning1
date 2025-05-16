e84bc308f0891ba905271c89aaa718901b8720ef
HenrikData <henrik.karlsson2.student@ksgyf.se>
2025-05-16 13:56:43 +0200

    snyggar till kod igen

--- Run_strukturverktyg.ps1
+++ Run_strukturverktyg.ps1
@@ -1,5 +1,3 @@
 # Denna fil skapar en mappstruktur och en loggfil baserat på det namn du väljer för huvudmappen.
 Import-Module "$PSScriptRoot\Modul_strukturverktyg.psm1"
 Starta-Strukturverktyg  
-
-# ← din huvudfunktion
