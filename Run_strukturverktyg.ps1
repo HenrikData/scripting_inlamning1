cc0d60db7c445b80df99b0d0cf4209a541923f33
HenrikData <henrik.karlsson2.student@ksgyf.se>
2025-05-16 13:10:46 +0200

    körfilen till strukturverktyg skapad

---
+++ Run_strukturverktyg.ps1
@@ -0,0 +1,5 @@
+# Denna fil skapar en mappstruktur och en loggfil baserat på det namn du väljer för huvudmappen.
+Import-Module "$PSScriptRoot\verktyg.psm1"
+Starta-Verktyg  
+
+# ← din huvudfunktion
